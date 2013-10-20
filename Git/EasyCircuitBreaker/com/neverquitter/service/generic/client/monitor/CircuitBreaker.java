aa9f471a7a193af911ccbf0a3634d9f933d557c9
vijay <vijay.ramcse@gmail.com>
2013-10-20 17:48:58 -0400

    initial commit

--- Git/EasyCircuitBreaker/com/neverquitter/service/generic/client/monitor/CircuitBreaker.java
+++
@@ -1,8 +0,0 @@
-package com.neverquitter.service.generic.client.monitor;
-
-import com.officedepot.service.generic.client.model.ServiceMonitor;
-
-public interface CircuitBreaker {
-	void setBrokenServiceTimer(ServiceMonitor serviceMonitor);
-	void setDurationToStop(long durationToStop);
-	}
